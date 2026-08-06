322c5e20928a68430a0506889eb26fee4515910e
Hadi Komail <114335211+hadi-komail@users.noreply.github.com>
2026-08-06 23:55:39 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const links = document.querySelector('.nav-links');
-toggle?.addEventListener('click', () => links.classList.toggle('open'));
-document.querySelectorAll('.nav-links a').forEach(a => a.addEventListener('click', () => links.classList.remove('open')));
